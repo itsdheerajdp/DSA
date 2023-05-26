59bfa71aea104f2f8c2d37d8e02aec29da4bf3fb
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-05-26 12:02:46 +0530

    unique_paths

---
+++ LeetCode/Unique_Paths.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int solve(int i,int j,int m,int n,vector<vector<int>>&dp){
+        if(i>=m || j>=n)return 0;
+        if(i==m-1 && j==n-1)return 1;
+        if(dp[i][j]!=-1)return dp[i][j];
+        dp[i][j]=solve(i,j+1,m,n,dp)+solve(i+1,j,m,n,dp);
+        return dp[i][j];
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector<int>(n,-1));
+        return solve(0,0,m,n,dp);
+    }
+};
