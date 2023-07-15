afd8ff90af6780c8bd7728308b3b4ed307ca2c8c
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-07-15 16:55:22 +0530

    c

---
+++ LeetCode/Longest_common_subsequence.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int solve(string &text1,string &text2,int i,int j,vector<vector<int>>&dp){
+        if(i==text1.length()) return 0;
+        if(j==text2.length())return 0;
+        int ans=0;
+        if(dp[i][j]!=-1)return dp[i][j];
+        if(text1[i]==text2[j])ans=1+solve(text1,text2,i+1,j+1,dp);
+        else ans=max(solve(text1,text2,i+1,j,dp),solve(text1,text2,i,j+1,dp));
+        return dp[i][j]=ans;  
+    }
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>>dp(text1.length(),vector<int>(text2.length(),-1));
+        return solve(text1,text2,0,0,dp);
+    }
+};
