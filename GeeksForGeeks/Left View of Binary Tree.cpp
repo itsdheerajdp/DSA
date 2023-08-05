3533caa7172db7993f9129b35f3f62a5d68696a6
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-08-05 11:36:20 +0530

    c

---
+++ GeeksForGeeks/Left View of Binary Tree.cpp
@@ -0,0 +1,16 @@
+void solve(Node* root,vector<int>&ans,int level){
+    if(root==NULL)return;
+    if(level==ans.size())ans.push_back(root->data);
+    solve(root->left,ans,level+1);
+    solve(root->right,ans,level+1);
+}
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   vector<int>ans;
+   if(root==NULL)
+       return ans;
+   solve(root,ans,0);
+   return ans;
+}
