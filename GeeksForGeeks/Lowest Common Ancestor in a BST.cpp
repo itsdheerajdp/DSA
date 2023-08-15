088b0755aacbfa27ed55d1c7b9e62d6bcd050d63
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-08-15 23:45:54 +0530

    C

---
+++ GeeksForGeeks/Lowest Common Ancestor in a BST.cpp
@@ -0,0 +1,14 @@
+class Solution{
+    public:
+        Node* LCA(Node *root, int n1, int n2)
+        {
+            // code here
+            if(root==NULL)return NULL;
+            if(root->data <n1 && root->data<n2)return LCA(root->right,n1,n2);
+            else if(root->data>n1 && root->data >n2)return LCA(root->left,n1,n2);
+           else return root;
+           
+           
+        }
+
+};
