57163713cc5fc16c606780184a29e0b94202a5e3
decodersingh <107138786+decodersingh@users.noreply.github.com>
2023-09-18 13:16:49 +0530

    18-09-2023

---
+++ GeeksForGeeks/Minimum Cost of ropes.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+    //Function to return the minimum cost of connecting the ropes.
+    long long minCost(long long arr[], long long n) {
+        // Your code here
+        priority_queue <long long, vector<long long>, greater<long long> > pq;
+        for(long long i=0;i<n;i++)pq.push(arr[i]);
+        long long ans=0;
+        while(pq.size()>1){
+            long long a=pq.top();
+            pq.pop();
+            long long b=pq.top();
+            pq.pop();
+            ans+=a+b;
+            pq.push(a+b);
+        }
+        return ans;
+    }
+};
